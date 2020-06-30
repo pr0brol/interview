a4de83e6caad5175c031bd14f5da403bedd89ad4
pr0brol <pr0_brol@mail.ru>
2020-06-30 20:50:45 +0300

    hw-6

--- src/main/java/com/geekbrains/lesson_6/SimpleServlet.java
+++ src/main/java/com/geekbrains/lesson_6/SimpleServlet.java
@@ -1,6 +1,5 @@
 package com.geekbrains.lesson_6;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,7 @@ import java.io.PrintWriter;
 public class SimpleServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter out = resp.getWriter();
         out.print("<h1>Hello World!</h1>");
     }
